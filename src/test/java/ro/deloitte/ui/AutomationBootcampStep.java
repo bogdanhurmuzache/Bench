5f65d75f36c89ec1df01770f6d5d1cbb2e0b32eb
bhurmuzache <bhurmuzache@deloittece.com>
2023-07-31 11:53:24 +0300

    Third commit

--- src/test/java/ro/deloitte/ui/AutomationBootcampStep.java
+++ src/test/java/ro/deloitte/ui/AutomationBootcampStep.java
@@ -8,6 +8,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class AutomationBootcampStep {
 
     private WebDriver driver;
