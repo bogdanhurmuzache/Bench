a931f5f4409d9280b8736ed5f893a66b029a75c9
bhurmuzache <bhurmuzache@deloittece.com>
2023-07-31 21:52:41 +0300

    Commit 31 July 21:52

--- src/test/java/ro/deloitte/ui/Bench.java
+++ src/test/java/ro/deloitte/ui/Bench.java
@@ -42,6 +42,9 @@ public class Bench {
 
 
     public static void main(String[] args) {
+
+                                        // Cmments
+
         //System.out.println("QA Automation");
         //System.out.println("Something");
         //System.out.println("13");
@@ -67,13 +70,87 @@ public class Bench {
         //System.out.print("Second line.");
         //System.out.println("STOP");
 
-        int myFirstNumber = 10;
+
+
+                                            // Variables
+
+        /*int myFirstNumber = 10;
         System.out.println("myFirstNumber");
         System.out.println(myFirstNumber);
         myFirstNumber = 13;
-        //System.out.println(myFirstNumber);
+        System.out.println(myFirstNumber);
+        myFirstNumber = 10 + 13;
+        System.out.println(myFirstNumber);
+        myFirstNumber = (10 + 10) + (13 * 10);
+        System.out.println(myFirstNumber);*/
+
+        /*int myFirstNumber = (10 + 10) + (13 * 10);
+        System.out.println(myFirstNumber);
+
+        int mySecondNumber = 12;
+        System.out.println(mySecondNumber);
+
+        int myThirdNumber = 6;
+        System.out.println(myThirdNumber);
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println((myTotal));
+
+        myThirdNumber = myFirstNumber * 2;
+        System.out.println(myThirdNumber);
+
+        myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println((myTotal));*/
 
 
+
+                                                    // Primitive Types
+
+        /*int myValue = 10000;
+        System.out.println(myValue);
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        System.out.println(myMinIntValue);
+
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println(myMaxIntValue);*/
+
+                                                    // Floating Point Precision
+
+        /*int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+
+        System.out.println(myIntValue);
+        System.out.println(myFloatValue);
+        System.out.println(myDoubleValue);
+
+        myIntValue = 5 / 2;
+        System.out.println(myIntValue);
+
+        myFloatValue = 5f / 2f;
+        System.out.println(myFloatValue);
+
+        myDoubleValue = 5d / 2d;
+        System.out.println(myDoubleValue);*/
+
+
+                                                    // Operators, Operands and Expressions
+
+        int results = 1 + 2;
+        System.out.println(results);
+
+        int previousResults = results;
+        System.out.println(results);
+
+        results = results - 1;
+        System.out.println(results);
+
+        System.out.print("previousResults = " + previousResults);
+// SPACE ????
+        int resultsA = 10;
+        resultsA++;
+        System.out.println(resultsA);
     }
 }
 
