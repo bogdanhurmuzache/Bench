2f4c07b0a4cb881b2627d9dce2e63ebad5f69df0
bhurmuzache <bhurmuzache@deloittece.com>
2023-08-09 09:12:16 +0300

    Commit 09.08 09:12

---
+++ src/test/java/ro/deloitte/ui/For_While.java
@@ -0,0 +1,52 @@
+package ro.deloitte.ui;
+
+public class For_While {
+    public static void main(String[] args) {
+
+        /*for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            System.out.print( );
+        }*/
+
+        /*for (int a = 0; a < 5; a = a + 2) {
+            System.out.println(a);
+        }*/
+
+        /*for (int a = 9; a >= 0; a--) {
+            for (int b = 0; b <= 9; b++){
+                System.out.println(b);
+            }
+            System.out.println();
+        }*/
+
+        /*for (int a = 0; a <= 9; a++) {
+            System.out.printf("row %d ", a);
+            for (int b = 0; b <= 9; b++){
+                System.out.println(b);
+            }
+            System.out.println();
+        }*/
+
+
+
+            /*int z = 0;
+            do {
+                System.out.println(z);
+                z++;
+            }
+            while (z < 10);*/
+
+
+
+        /*for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                //continue;
+                break;
+            }
+            System.out.println(i);
+        }*/
+
+
+
+    }
+}
